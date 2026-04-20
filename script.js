4ef3dbbc50939a49252f80af6dc6c8580e844ed5
yuell123 <142213998+yuell123@users.noreply.github.com>
2026-04-20 18:34:52 +0700

    Add HTML, CSS, and JS files for a birthday celebration

---
+++ script.js
@@ -0,0 +1,9 @@
+function celebrate() {
+    const title = document.getElementById('title');
+    const message = document.getElementById('message');
+    
+    title.innerText = "HBD Buat Kamu! ✨";
+    message.innerText = "Semoga Panjang Umur Dan Sehat Selalu!";
+    
+    alert("🎉 Selamat Ulang Tahun! 🎉");
+}
